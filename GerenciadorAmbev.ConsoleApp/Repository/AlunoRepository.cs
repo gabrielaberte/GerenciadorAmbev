dc5b17f6a0e829b8d5aa756fbd268461a495f5ad
Gabe Berté <gabriela.berte@ambevtech.com.br>
2024-07-19 14:49:57 -0300

    criando repository e novo proj console

---
+++ GerenciadorAmbev.ConsoleApp/Repository/AlunoRepository.cs
@@ -0,0 +1,41 @@
+﻿using GerenciadorAmbev.ConsoleApp;
+using GerenciadorAmbev.Domain.Model;
+
+namespace GerenciadorAmbev.Data.Repository
+{
+    public class AlunoRepository
+    {
+        protected readonly Contexto contexto;
+        public AlunoRepository()
+        {
+            contexto = new Contexto();
+        }
+        public List<Aluno> SelecionarTudo()
+        {
+            return contexto.Aluno.ToList();
+        }
+
+        public void Incluir(Aluno aluno)
+        {
+            contexto.Aluno.Add(aluno);
+            contexto.SaveChanges();
+        }
+        public void Alterar(Aluno aluno)
+        {
+            contexto.Aluno.Update(aluno);
+            contexto.SaveChanges();
+        }
+        public Aluno Selecionar(int id)
+        {
+           return contexto.Aluno.FirstOrDefault(x => x.Id == id);
+        }
+        public void Excluir(int id)
+        {
+            var aluno = Selecionar(id);
+            contexto.Aluno.Remove(aluno);
+            contexto.SaveChanges();
+        }
+    }
+
+
+}
